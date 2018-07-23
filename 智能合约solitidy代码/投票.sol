bbe9ffd53d71bbb600257fb22366cb752b26c8f8
Liu <Liu@example.com>
2018-07-23 14:18:40 +0800

    更改的在remix可以通过的

--- 智能合约solitidy代码/投票.sol
+++ 智能合约solitidy代码/投票.sol
@@ -1,115 +1,128 @@
-pragma solidity ^0.4.0;
-contract voteDemo {
-    
-    //定义投票人的结构
-    struct Voter{
-        uint weight; //投票人的权重
-        bool voted ;// 是否已经投票
-        address delegate; //委托代理人投票
-        uint vote; // 投票主题的序号
-    }
-    
-    //定义投票主题的结构
-    struct Posposal{
-        bytes8 name ;  //投票主题的名字
-        uint voteCount; //主题的得到的票数
+pragma solidity ^0.4.22;
+
+/// @title 委托投票
+contract Ballot {
+    // 这里声明了一个新的复合类型用于稍后的变量
+    // 它用来表示一个选民
+    struct Voter {
+        uint weight; // 计票的权重
+        bool voted;  // 若为真，代表该人已投票
+        address delegate; // 被委托人
+        uint vote;   // 投票提案的索引
     }
 
-    //定义投票的发起者
+    // 提案的类型
+    struct Proposal {
+        bytes32 name;   // 简称（最长32个字节）
+        uint voteCount; // 得票数
+    }
+
     address public chairperson;
-    
-    //所有人的投票人
-    mapping(address=>Voter) public voters;
-    
-    //具体的投票主题
-    Posposal[] public posposals;
-    
-    
-    //构造函数
-    constructor (bytes8[] peposposalName)public{
-        //初始化投票的发起人，就是当前合约的部署者
+
+    // 这声明了一个状态变量，为每个可能的地址存储一个 `Voter`。
+    mapping(address => Voter) public voters;
+
+    // 一个 `Proposal` 结构类型的动态数组
+    Proposal[] public proposals;
+
+    /// 为 `proposalNames` 中的每个提案，创建一个新的（投票）表决
+    constructor(bytes32[] proposalNames) public {
         chairperson = msg.sender;
-        //给发起人投票权
         voters[chairperson].weight = 1;
-        
-        //初始化投票的主题
-        for(uint i = 0; i < peposposalName.length ;i++ ){
-            posposals.push(Posposal({name:peposposalName[i],voteCount:0}));
+        //对于提供的每个提案名称，
+        //创建一个新的 Proposal 对象并把它添加到数组的末尾。
+        for (uint i = 0; i < proposalNames.length; i++) {
+            // `Proposal({...})` 创建一个临时 Proposal 对象，
+            // `proposals.push(...)` 将其添加到 `proposals` 的末尾
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
         }
     }
-    
-    //添加投票者
-    function  giveRightToVote(address _voter)public{
-        //只有投票的发起人才能够添加投票者
-        //添加的投票者不能是已经参加过投票了
-        
-        require(voters[_voter].voted == false);
-        require(msg.sender==chairperson);
-        //赋予合格的投票者投票权重
-        voters[_voter].weight = 1;
+
+    // 授权 `voter` 对这个（投票）表决进行投票
+    // 只有 `chairperson` 可以调用该函数。
+    function giveRightToVote(address voter) public {
+        // 若 `require` 的第一个参数的计算结果为 `false`，
+        // 则终止执行，撤销所有对状态和以太币余额的改动。
+        // 在旧版的 EVM 中这曾经会消耗所有 gas，但现在不会了。
+        // 使用 require 来检查函数是否被正确地调用，是一个好习惯。
+        // 你也可以在 require 的第二个参数中提供一个对错误情况的解释。
+        require(
+            msg.sender == chairperson,
+            "Only chairperson can give right to vote."
+        );
+        require(
+            !voters[voter].voted,
+            "The voter already voted."
+        );
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;
     }
-    
-    //将自己的票委托给to来投
-    function  delegate(address dele)public{
-        //检查当前交易的发起者是不是已经投过票了
-        Voter sender = voters[msg.sender];
-        //如果是的话，则程序终止
-        require(sender.voted == false);
-        
-        //检查委托人是不是也委托人其他人来投票
-        while(voters[dele].delegate != address(0)){
-            //如果是的话，则把委托人设置成委托人的委托人
-            address add = voters[dele].delegate;
-            //如果发现最终的委托人是自己，则终止程序
-            require(add != msg.sender);
+
+    /// 把你的投票委托到投票者 `to`。
+    function delegate(address to) public {
+        // 传引用
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "You already voted.");
+
+        require(to != msg.sender, "Self-delegation is disallowed.");
+
+        // 委托是可以传递的，只要被委托者 `to` 也设置了委托。
+        // 一般来说，这种循环委托是危险的。因为，如果传递的链条太长，
+        // 则可能需消耗的gas要多于区块中剩余的（大于区块设置的gasLimit），
+        // 这种情况下，委托不会被执行。
+        // 而在另一些情况下，如果形成闭环，则会让合约完全卡住。
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+
+            // 不允许闭环委托
+            require(to != msg.sender, "Found loop in delegation.");
         }
-        
-        //交易的发起者不能再投票了
+
+        // `sender` 是一个引用, 相当于对 `voters[msg.sender].voted` 进行修改
         sender.voted = true;
-        //设置交易的发起者的投票代理人
-        sender.delegate = add;
-        //找到代理人
-        Voter delegate = voters[add];
-        //检测代理人是否已经投票
-        if(delegate.voted){
-            //如果是：则把票直接投给代理人投的那个主题
-            posposals[delegate.vote].voteCount += sender.weight;
-        }else{
-            //如果不是：则把投票的权重给予代理人
-            delegate.weight += sender.weight;
+        sender.delegate = to;
+        Voter storage delegate_ = voters[to];
+        if (delegate_.voted) {
+            // 若被委托者已经投过票了，直接增加得票数
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            // 若被委托者还没投票，增加委托者的权重
+            delegate_.weight += sender.weight;
         }
     }
-    //投票
-    function vote(uint pid)public{
-        //找到投票者
-        Voter sender = voters[msg.sender];
-        //检查是不是已经投过票
-        require(sender.voted==false);
-            //如果否：则投票
-        sender.voted = true;  //设置当前用户已投票
-        sender.vote = pid;    //设置当前用户的投的主题的编号
-        posposals[pid].voteCount += sender.weight;  //把当前用户的投票权重给予对应的主题
+
+    /// 把你的票(包括委托给你的票)，
+    /// 投给提案 `proposals[proposal].name`.
+    function vote(uint proposal) public {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "Already voted.");
+        sender.voted = true;
+        sender.vote = proposal;
+
+        // 如果 `proposal` 超过了数组的范围，则会自动抛出异常，并恢复所有的改动
+        proposals[proposal].voteCount += sender.weight;
     }
-   
-    
-    //计算票数最多的主题
-    function winid() view public returns(uint winningid){
-        //声明一个临时变量，用来比大小
-        uint winningCount = 0;
-        //编列主题，找到投票数最大的主题
-        for(uint i = 0; i<posposals.length; i++ ){
-            if(posposals[i].voteCount > winningCount ){
-                winningCount = posposals[i].voteCount;
-                winningid = i;
+
+    /// @dev 结合之前所有的投票，计算出最终胜出的提案
+    function winningProposal() public view
+            returns (uint winningProposal_)
+    {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
             }
         }
     }
-    
-    
-    function winname() view public returns(bytes8 winnername){
-        winnername = posposals[winid()].name;
+
+    // 调用 winningProposal() 函数以获取提案数组中获胜者的索引，并以此返回获胜者的名称
+    function winnerName() public view
+            returns (bytes32 winnerName_)
+    {
+        winnerName_ = proposals[winningProposal()].name;
     }
-    
-    
-    
 }
